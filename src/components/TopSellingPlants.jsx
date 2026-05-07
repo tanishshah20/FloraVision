f652235a29413a786a1a8c20b2b48771cf15344a
tanishshah20 <shahtanish207@gmail.com>
2026-05-08 01:59:06 +0530

    Update

--- src/components/TopSellingPlants.jsx
+++ src/components/TopSellingPlants.jsx
@@ -9,7 +9,7 @@ const TopSellingPlants = () => {
       <div className="max-w-7xl mx-auto">
         <SectionTitle title="Our Top Selling Plants" background='linear-gradient(316.11deg, rgba(251, 211, 0, 0.5) 0.4%, rgba(255, 255, 255, 0.5) 110.43%)'/>
         
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-20 mt-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-32 mt-32">
           {plantsData.map((plant) => (
             <PlantCard key={plant.id} plant={plant} />
           ))}
